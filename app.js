607041268377fa276d674a5e160dcd853891a389
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-08 12:15:11 -0500

    Added function to obtain player choice

--- app.js
+++ app.js
@@ -3,16 +3,26 @@
 function randomChoice() {
     const randomValue = Math.random();
     if (randomValue < 0.33) {
-        return "rock"
+        return "rock";
     }
     else if (randomValue > 0.66) {
-        return "paper"
+        return "paper";
     }
     else {
-        return "scissors"
+        return "scissors";
     }
 }
 
 function getComputerChoice() {
     return randomChoice();
 }
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    const humanChoice = prompt("Please enter: rock, paper, or scissors.");
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
+
