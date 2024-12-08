06bcd2c8ad4722a2301b799ed4bd446dbb02ecfe
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-08 11:58:22 -0500

    Added logic to get the computer's choice

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // app.js: Logic for rock-paper-scissors game
+
+function randomChoice() {
+    const randomValue = Math.random();
+    if (randomValue < 0.33) {
+        return "rock"
+    }
+    else if (randomValue > 0.66) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function getComputerChoice() {
+    return randomChoice();
+}
