270567e3a8ca707cc669e5c39c957ed097da5fc9
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-08 16:47:10 -0500

    Added playRound function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // app.js: Logic for rock-paper-scissors game
 
+let humanScore = 0;
+let computerScore = 0;
+
 function randomChoice() {
     const randomValue = Math.random();
     if (randomValue < 0.33) {
@@ -17,12 +20,53 @@ function getComputerChoice() {
     return randomChoice();
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     const humanChoice = prompt("Please enter: rock, paper, or scissors.");
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(computerChoice, humanChoice) {
 
+    humanChoice = humanChoice.toLowerCase();
+
+    if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore = humanScore + 1;
+        return console.log("You win! Rock beats scissors.");
+    }
+    else if (computerChoice === "rock" && humanChoice === "paper") {
+        humanScore = humanScore + 1;
+        return console.log("You win! Paper beats rock.");
+    }
+    else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore = humanScore + 1;
+        return console.log("You win! Scissors beats paper.");
+    }
+    else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore = computerScore + 1;
+        return console.log("You lose! Rock beats scissors.");
+    }
+    else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore = computerScore + 1;
+        return console.log("You lose! Paper beats rock.");
+    }
+    else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore = computerScore + 1;
+        return console.log("You lose! Scissors beats paper.");
+    }
+    else if (computerChoice === "rock" && humanChoice === "rock") {
+        return console.log("Draw! Please play again.");
+    }
+    else if (computerChoice === "paper" && humanChoice === "paper") {
+        return console.log("Draw! Please play again.");
+    }
+    else if (computerChoice === "scissors" && humanChoice === "scissors") {
+        return console.log("Draw! Please play again.");
+    }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+console.log(computerSelection);
+
+    playRound(computerSelection, humanSelection);
